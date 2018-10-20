0d6c0f25ba64cc569e8669a1421189b57f511e14
Gregory Kan <kan.gregory@gmail.com>
2018-10-20 18:27:28 +1300

    feat(options): prevent an option from belonging to more than one question

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,19 @@
 class Link < ApplicationRecord
   belongs_to :source, foreign_key: :source_id, class_name: "Node"
   belongs_to :target, foreign_key: :target_id, class_name: "Node"
+  before_create :prevent_option_from_belonging_to_more_than_one_question
+
+  def prevent_option_from_belonging_to_more_than_one_question
+    target_node = Node.find(self.target_id)
+    if target_node.node_type == 'option'
+      has_existing_source_nodes = Node.where(target_id: self.target_id).any?
+      if has_existing_source_nodes
+        return false
+      else
+        return true
+      end
+    else
+      return true
+    end
+  end
 end
