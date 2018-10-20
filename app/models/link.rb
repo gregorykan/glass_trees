b1a2e2f72183218b0a102264967b3a899a14b452
Gregory Kan <kan.gregory@gmail.com>
2018-10-20 18:50:21 +1300

    chore(validations): added validation for links so that link can't be created if option already belongs to a question

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,19 +1,15 @@
 class Link < ApplicationRecord
   belongs_to :source, foreign_key: :source_id, class_name: "Node"
   belongs_to :target, foreign_key: :target_id, class_name: "Node"
-  before_create :prevent_option_from_belonging_to_more_than_one_question
+  validate :prevent_option_from_belonging_to_more_than_one_question
 
   def prevent_option_from_belonging_to_more_than_one_question
     target_node = Node.find(self.target_id)
     if target_node.node_type == 'option'
-      has_existing_source_nodes = Node.where(target_id: self.target_id).any?
+      has_existing_source_nodes = Link.where(target_id: self.target_id).any?
       if has_existing_source_nodes
-        return false
-      else
-        return true
+        errors.add(:base, "An option can't belong to more than one question")
       end
-    else
-      return true
     end
   end
 end
