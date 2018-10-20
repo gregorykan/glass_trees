90390a223a0fa6540d53bbe801c6bc04d808f1f6
Gregory Kan <kan.gregory@gmail.com>
2018-10-20 17:34:17 +1300

    feat(nodes): node many-to-many self-join with links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,4 @@
 class Link < ApplicationRecord
+  belongs_to :precedent, foreign_key: :precedent_id, class_name: "Node"
+  belongs_to :consequent, foreign_key: :consequent_id, class_name: "Node"
 end
--- app/models/node.rb
+++ app/models/node.rb
@@ -1,2 +1,7 @@
 class Node < ApplicationRecord
+  has_many :precedent_links, foreign_key: :consequent_id, class_name: "Link"
+  has_many :precedents, through: :precedent_links, source: :precedent
+
+  has_many :consequent_links, foreign_key: :precedent_id, class_name: "Link"
+  has_many :consequents, through: :consequent_links, source: :consequent
 end
