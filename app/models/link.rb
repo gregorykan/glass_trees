39de3c5af479e7fc54d76178957e1e1777481d71
Gregory Kan <kan.gregory@gmail.com>
2018-10-20 19:02:35 +1300

    fix(nodes): 'inverted' the direction of the relationship between option and question node types

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,9 +4,9 @@ class Link < ApplicationRecord
   validate :prevent_option_from_belonging_to_more_than_one_question
 
   def prevent_option_from_belonging_to_more_than_one_question
-    target_node = Node.find(self.target_id)
-    if target_node.node_type == 'option'
-      has_existing_source_nodes = Link.where(target_id: self.target_id).any?
+    source_node = Node.find(self.source_id)
+    if source_node.node_type == 'option'
+      has_existing_source_nodes = Link.where(source_id: self.source_id).any?
       if has_existing_source_nodes
         errors.add(:base, "An option can't belong to more than one question")
       end
