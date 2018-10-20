da8cfae9bee02634678abbb3dfd165d9ac0b66b1
Gregory Kan <kan.gregory@gmail.com>
2018-10-21 12:51:48 +1300

    chore(refactor): got rid of unused code copied over

--- client/src/bundles/index.js
+++ client/src/bundles/index.js
@@ -1,31 +1,10 @@
 import { composeBundles, debugBundle, createUrlBundle } from 'redux-bundler'
 
-// import accountsBundle from './accounts'
-// import ordersBundle from './orders'
-// import notificationsBundle from './notifications'
-// import myProfileBundle from './myProfile'
-// import invitationsBundle from './invitations'
-// import suppliersBundle from './suppliers'
-// import groupsBundle from './groups'
-// import productsBundle from './products'
-// import wantsBundle from './wants'
-// import lineItemsBundle from './lineItems'
-
 import routes from './routes'
 
 import extraArgs from './extra-args'
 
 export default composeBundles(
-  // accountsBundle,
-  // ordersBundle,
-  // notificationsBundle,
-  // myProfileBundle,
-  // invitationsBundle,
-  // suppliersBundle,
-  // groupsBundle,
-  // productsBundle,
-  // wantsBundle,
-  // lineItemsBundle,
   createUrlBundle(),
   debugBundle,
   extraArgs,
