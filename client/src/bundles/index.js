37c5bf8a0bfe2097205056d92667d0aab3522d04
Gregory Kan <kan.gregory@gmail.com>
2018-10-21 13:49:58 +1300

    feat(links): added links bundle with basic fetch

--- client/src/bundles/index.js
+++ client/src/bundles/index.js
@@ -4,9 +4,11 @@ import routes from './routes'
 import extraArgs from './extra-args'
 
 import nodesBundle from './nodes'
+import linksBundle from './links'
 
 export default composeBundles(
   nodesBundle,
+  linksBundle,
   createUrlBundle(),
   debugBundle,
   extraArgs,
