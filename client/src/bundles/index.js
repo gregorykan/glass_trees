b0e993d22d46311dea666e60e7ee8184ed7f5143
Gregory Kan <kan.gregory@gmail.com>
2018-10-21 13:48:28 +1300

    feat(nodes): added nodes bundle with basic fetch

--- client/src/bundles/index.js
+++ client/src/bundles/index.js
@@ -1,10 +1,12 @@
 import { composeBundles, debugBundle, createUrlBundle } from 'redux-bundler'
 
 import routes from './routes'
-
 import extraArgs from './extra-args'
 
+import nodesBundle from './nodes'
+
 export default composeBundles(
+  nodesBundle,
   createUrlBundle(),
   debugBundle,
   extraArgs,
---
+++ client/src/bundles/nodes.js
@@ -0,0 +1,49 @@
+import { createAsyncResourceBundle, createSelector } from 'redux-bundler'
+import cuid from 'cuid'
+import { omit, concat, isNil, find, filter } from 'lodash'
+import ms from 'milliseconds'
+
+const bundle = createAsyncResourceBundle({
+  name: 'nodes',
+  getPromise: async ({ apiFetch, getState }) => {
+    return apiFetch(`api/v1/nodes`, {})
+      .then(response => response.json())
+      .catch(err => {
+        console.log('err', err)
+        throw new Error({ permanent: true })
+      })
+  },
+  staleAfter: ms.minutes(5),
+  retryAfter: ms.minutes(2)
+})
+
+const initialState = {
+  // needed by createAsyncResourceBundle
+  data: null,
+  errorTimes: [],
+  errorType: null,
+  failedPermanently: false,
+  isExpired: false,
+  isLoading: false,
+  isOutdated: false,
+  lastSuccess: null
+}
+
+const baseReducer = bundle.reducer
+bundle.reducer = (state = initialState, action) => {
+  return baseReducer(state, action)
+}
+
+// bundle.selectNodes = (state) => state.nodes.data
+
+bundle.reactNodesFetch = createSelector(
+  'selectNodesShouldUpdate',
+  (shouldUpdate) => {
+    if (shouldUpdate) {
+      return { actionCreator: 'doFetchNodes' }
+    }
+    return false
+  }
+)
+
+export default bundle
