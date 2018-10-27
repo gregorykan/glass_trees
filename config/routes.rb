cd45af2dea15a24c0503e71dc52f1065912c9ef0
Gregory Kan <kan.gregory@gmail.com>
2018-10-27 14:26:00 +1300

    feat(devise): devise token auth init

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
       resources :nodes
       resources :links
       post '/nodes/create_node', to: 'nodes#create_node'
