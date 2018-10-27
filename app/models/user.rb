c47e92c6396bd7d0457cbba39d00090eb48014a2
Gregory Kan <kan.gregory@gmail.com>
2018-10-27 15:22:47 +1300

    feat(invitable): devise-invitable init

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
   belongs_to :group
