422de876f86d24af98023b518c48d2da213747f7
Gregory Kan <kan.gregory@gmail.com>
2018-10-27 17:35:04 +1300

    chore(config): use 3000 for server

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3001' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   config.active_job.queue_adapter = :inline
