6b8f4dd41f64c6647b298ad643165acbf45a5b48
Gregory Kan <kan.gregory@gmail.com>
2018-10-27 14:27:37 +1300

    feat(devise): ran migration for users

--- db/migrate/20181027012443_devise_token_auth_create_users.rb
+++ db/migrate/20181027012443_devise_token_auth_create_users.rb
@@ -1,6 +1,6 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
   def change
-    
+
     create_table(:users) do |t|
       ## Required
       t.string :provider, :null => false, :default => "email"
@@ -17,12 +17,12 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
       ## Rememberable
       t.datetime :remember_created_at
 
-      ## Trackable
-      t.integer  :sign_in_count, :default => 0, :null => false
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
+      # ## Trackable
+      # t.integer  :sign_in_count, :default => 0, :null => false
+      # t.datetime :current_sign_in_at
+      # t.datetime :last_sign_in_at
+      # t.string   :current_sign_in_ip
+      # t.string   :last_sign_in_ip
 
       ## Confirmable
       t.string   :confirmation_token
