fd639edad2de8d1e9a77b7076dc044f60f935eac
Gregory Kan <kan.gregory@gmail.com>
2018-11-12 15:10:28 +1300

    chore(refactor): removed semicolons

--- client/src/components/graph2.js
+++ client/src/components/graph2.js
@@ -10,87 +10,87 @@ const containerStyle = {
   alignItems: 'center'
 }
 
-var width = 800;
-var height = 600;
+var width = 800
+var height = 600
 var simulation = d3.forceSimulation()
-  .force('collide', d3.forceCollide(d => 2 * d.size))
+  // .force('collide', d3.forceCollide(d => 2 * d.size))
   .force('charge', d3.forceManyBody(-650))
   .force('center', d3.forceCenter(width / 2, height / 2))
-  .stop();
+  .stop()
 
 class Graph extends React.Component {
   // constructor(props) {
-  //   super(props);
-  //   this.state = {selected: null};
+  //   super(props)
+  //   this.state = {selected: null}
   //
-  //   this.selectNode = this.selectNode.bind(this);
+  //   this.selectNode = this.selectNode.bind(this)
   // }
 
   // shouldComponentUpdate(nextProps, nextState) {
   //   if (nextProps.version === this.props.version) {
   //     // if version is the same, no updates to data
   //     // so it must be interaction to select+highlight a node
-  //     this.calculateHighlights(nextState.selected);
+  //     this.calculateHighlights(nextState.selected)
   //     this.circles.attr('opacity', d =>
   //       !nextState.selected || this.highlightedNodes[d.key] ? 1 : 0.2)
   //     this.lines.attr('opacity', d =>
   //       !nextState.selected || this.highlightedLinks[d.key] ? 0.5 : 0.1)
-  //     return false;
+  //     return false
   //   }
-  //   return true;
+  //   return true
   // }
 
-  componentDidMount() {
-    this.container = d3.select(this.refs.container);
-    this.calculateData();
-    // this.calculateHighlights(this.state.selected);
-    this.renderLinks();
-    this.renderNodes();
+  componentDidMount () {
+    this.container = d3.select(this.refs.container)
+    this.calculateData()
+    // this.calculateHighlights(this.state.selected)
+    this.renderLinks()
+    this.renderNodes()
   }
 
   // componentDidUpdate() {
-  //   this.calculateData();
-  //   // this.calculateHighlights(this.state.selected);
-  //   this.renderLinks();
-  //   this.renderNodes();
+  //   this.calculateData()
+  //   // this.calculateHighlights(this.state.selected)
+  //   this.renderLinks()
+  //   this.renderNodes()
   // }
 
-  calculateData() {
+  calculateData () {
     // nodes and links are mutated in this function
     // they are given all the extra stuff that d3 needs to render them
-    var {nodes, links} = this.props;
+    var { nodes, links } = this.props
     simulation.nodes(nodes)
-      .force('link', d3.forceLink(links).id(d => d.id).distance(50));
+      .force('link', d3.forceLink(links).id(d => d.id).distance(50))
 
-    times(2000, () => simulation.tick());
+    times(2000, () => simulation.tick())
 
-    this.nodes = nodes;
-    this.links = links;
+    this.nodes = nodes
+    this.links = links
   }
 
   // calculateHighlights(selected) {
-  //   this.highlightedNodes = {};
-  //   this.highlightedLinks = {};
+  //   this.highlightedNodes = {}
+  //   this.highlightedLinks = {}
   //   if (selected) {
-  //     this.highlightedNodes[selected] = 1;
+  //     this.highlightedNodes[selected] = 1
   //     _.each(this.links, link => {
   //       if (link.source.key === selected) {
-  //         this.highlightedNodes[link.target.key] = 1;
-  //         this.highlightedLinks[link.key] = 1;
+  //         this.highlightedNodes[link.target.key] = 1
+  //         this.highlightedLinks[link.key] = 1
   //       }
   //       if (link.target.key === selected) {
-  //         this.highlightedNodes[link.source.key] = 1;
-  //         this.highlightedLinks[link.key] = 1;
+  //         this.highlightedNodes[link.source.key] = 1
+  //         this.highlightedLinks[link.key] = 1
   //       }
-  //     });
+  //     })
   //   }
   // }
 
-  renderNodes() {
+  renderNodes () {
     this.nodeElements = this.container.selectAll('g')
-      .data(this.nodes, d => d.id);
+      .data(this.nodes, d => d.id)
     // exit
-    this.nodeElements.exit().remove();
+    this.nodeElements.exit().remove()
     // enter + update
     this.nodeElements = this.nodeElements.enter().append('g')
       .classed('node', true)
@@ -103,18 +103,18 @@ class Graph extends React.Component {
       .attr('opacity', d => 1)
       // .attr('opacity', d =>
       //   !this.state.selected || this.highlightedNodes[d.key] ? 1 : 0.2)
-      // .on('click', this.selectNode);
+      // .on('click', this.selectNode)
     this.nodeElements.append('text')
       .attr('dx', d => d.x - 10)
       .attr('dy', d => d.y + 20)
       .text(d => d.label)
   }
 
-  renderLinks() {
+  renderLinks () {
     this.linkElements = this.container.selectAll('line')
-      .data(this.links, d => d.id);
+      .data(this.links, d => d.id)
     // exit
-    this.linkElements.exit().remove();
+    this.linkElements.exit().remove()
     // enter + update
     this.linkElements = this.linkElements.enter().insert('line', 'circle')
       .classed('link', true)
@@ -127,18 +127,18 @@ class Graph extends React.Component {
       .attr('y2', d => d.target.y)
       .attr('opacity', d => 1)
       // .attr('opacity', d =>
-      //   !this.state.selected || this.highlightedLinks[d.key] ? 0.5 : 0.1);
+      //   !this.state.selected || this.highlightedLinks[d.key] ? 0.5 : 0.1)
   }
 
   // selectNode(node) {
   //   if (node.key === this.state.selected) {
-  //     this.setState({selected: null});
+  //     this.setState({selected: null})
   //   } else {
-  //     this.setState({selected: node.key});
+  //     this.setState({selected: node.key})
   //   }
   // }
 
-  render() {
+  render () {
     return (
       <div style={containerStyle}>
         <svg style={{ backgroundColor: 'pink', height: 600, width: 800 }} ref='container' />
